35154534a8b22e00f0674a8363e8c289ce2b27e1
Gopiya2000 <gopiya2000@gmail.com>
2022-05-30 21:51:31 +0530

    Commit

---
+++ Error-Handling/error.js
@@ -0,0 +1,11 @@
+var fs = require("fs");
+fs.readFile("data.txt","UTF-8",function(error,data){
+    //change file name and check for uncaught exception
+    if(error){
+        throw error;
+    }
+    console.log(data);
+});
+process.on("uncaughtException",function(error){
+    console.log("The exception was caught")
+});
