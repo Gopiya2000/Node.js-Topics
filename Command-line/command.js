e86c197ea501f785d24e41f6bbefa63f4fd79d65
Gopiya <gopiya2000@gmail.com>
2022-06-15 14:36:24 +0530

    Commit

--- Command-line/command.js
+++ Command-line/command.js
@@ -1,4 +1,6 @@
-let arg = process.argv;
-console.log(arg)
-let res = arg.filter((value)=>value % 2 == 0)
-console.log(res)
+// let arg = process.argv;
+// console.log(arg)
+// let res = arg.filter((value)=e % 2 >valu== 0)
+// console.log(res)
+var value = process.argv.slice(2);
+console.log(value);
